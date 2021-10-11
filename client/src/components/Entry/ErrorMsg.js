ab420ac78b9134a1fe3c6a6228913314248e5d27
demac44 <demirumejr2@gmail.com>
2021-10-12 01:29:38 +0300

    Register form validation

--- client/src/components/Entry/ErrorMsg.js
+++ client/src/components/Entry/ErrorMsg.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
 import '../../App.css'
+import '../../General.css'
 
 const ErrorMsg = ({message}) => {
     return (
-        <div className='error-msg'><p>{message}</p></div>
+        <div className='error-msg-box flex-ctr'>
+            <h6 className='error-msg'>{message}</h6>
+        </div>
     )
 }
 
--- client/src/components/Entry/Register/RegisterForm.js
+++ client/src/components/Entry/Register/RegisterForm.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import gql from 'graphql-tag';
 import {useQuery, useMutation } from 'react-apollo';
+import ErrorMsg from '../ErrorMsg'
 
 const USER_QUERY = gql`
     query {
@@ -43,31 +44,70 @@ const CREATE_USER = gql`
 
     
 const RegisterForm = () => {
-    let tag_name, first_name, last_name, year, day,email, month, password;
+    let tag_name, first_name, last_name, year, day,email, month, password, confirmpass;
     const [new_user] = useMutation(CREATE_USER)
-    const [emailErr, setEmailErr] = useState(false)
-    const [usernameErr, setUsernameErr] = useState(false)
-
-        
     const {data, loading, error} = useQuery(USER_QUERY)
+    
+    const [selectYear, setSelectYear] = useState([])
+    const [selectDay, setSelectDay] = useState([])
+    
+    const [usernameNotFound, setUsernameNotFound] = useState(false)
+    const [emailNotFound, setEmailNotFound] = useState(false)
+    const [emailNotValid, setEmailNotValid] = useState(false)
+    const [namesNotValid, setNamesNotValid] = useState(false)
+    const [usernameNotValid, setUsernameNotValid] = useState(false)
+    const [ageError, setAgeError] = useState(false)
+    const [passNotValid, setPassNotValid] = useState(false)
+    const [confirmErr, setConfirmErr] = useState(false)
+    const [errorMsg, setErrorMsg] = useState('')
+
+    useEffect(()=>{
+        let years = []
+        let days = []
+        for(let i=2021;i>1920;i--){
+            years.push(`${i}`)
+        }
+        for(let i=1;i<32;i++){
+            if (i < 10) days.push('0'+i)
+            else days.push(`${i}`)
+        }
+        setSelectYear(years)
+        setSelectDay(days)
+    }, [])
+
+    const setErrorsFalse = () => {
+        setEmailNotFound(false)
+        setUsernameNotFound(false)
+        setEmailNotValid(false)
+        setUsernameNotValid(false)
+        setNamesNotValid(false)
+        setAgeError(false)
+        setPassNotValid(false)
+        setConfirmErr(false)
+        setErrorMsg('')
+    }
 
     if (loading) return loading
-    if (error) return error
-
+    if (error) console.log(error)
 
     const checkEmail = (email) => {
         let email_found = false
-        data.users.map(user => {
-            if(user.email === email){
+        data.users.forEach(user => {
+            if(user.email === email.toLowerCase()){
                 email_found = true
                 return user
             }
         })
         return email_found
     }
+    const validateEmail = (email) => {
+        let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        return re.test(email.toLowerCase())
+    }
+
     const checkUsername = (username) => {
         let username_found = false
-        data.users.map(user => {
+        data.users.forEach(user => {
             if(user.tag_name === username){
                 username_found = true
                 return user
@@ -76,29 +116,101 @@ const RegisterForm = () => {
         return username_found
     }
 
+    const validateUsername = (username) => {
+        let username_valid = true
+        let chars = "._abcdefghijklmnopqrstuvwxyz0123456789"
+        for(let i=0;i<username.length;i++){
+            if (!chars.includes(username[i]) || username.length > 30){
+                username_valid = false
+                return
+            }
+        }
+        return username_valid
+    }
+
+    const validateNames = (fname, lname) => {
+        let error = false
+        let chars = "<>@!#$%^&*()_+[]{}?:;|'\"\\,./~`-=0123456789"
+        for(let i = 0;i<fname.length;i++){
+            if (chars.includes(fname[i]) || fname[i] === ' '){
+                error=true
+            }
+        }
+        for(let i = 0;i<lname.length;i++){
+            if (chars.includes(lname[i])){
+                error=true
+            }
+        }
+        return error
+    }
+
+    const validateAge = (birth_date) => {
+        return Math.floor((new Date() - new Date(birth_date).getTime()) / 3.15576e+10)
+    }
+
+    const validatePassword = (password) => {
+         let validPass = true
+         if (password.length < 8 || password.length > 30){
+             validPass = false
+             return validPass
+         }
+         for(let i = 0;i<password.length;i++){
+             if(password[i] === ' '){
+                 validPass = false
+                 break
+             }
+         }
+         return validPass
+    }
+
+    const confirmPass = (password, confirmpass) => {
+        let confirm = true
+        if (password !== confirmpass){
+            confirm = false
+        }
+        return confirm
+    }
+
     const handleSubmit = async (e) => {
         e.preventDefault()
         let birth_date = `${year.value+'-'+month.value+'-'+day.value}`
+        setErrorsFalse()
 
-        if (checkEmail(email.value)){
-            setEmailErr(true)
-            setUsernameErr(false)
+        if (validateNames(first_name.value, last_name.value)){
+            setNamesNotValid(true)
+            setErrorMsg('First name and last name can contain only letters, not numbers, whitespace or any other special characters!')
+        } else if (validateAge(birth_date) < 13) {
+            setAgeError(true)
+            setErrorMsg('You must be at least 13 years old to register')
+        } else if (!validateEmail(email.value)) {
+            setEmailNotValid(true)
+            setErrorMsg('Email not valid')
+        } else if (!validateUsername(tag_name.value)){
+            setUsernameNotValid(true)
+            setErrorMsg('Username must contains only lowercase letters, numbers, underscores and dots and cannot be longer than 30 characters')
+        } else if (!validatePassword(password.value)){
+            setPassNotValid(true)
+            setErrorMsg('Password must be between 8 and 30 characters long and should not contain whitespace')
+        } else if (!confirmPass(password.value, confirmpass.value)) {
+            setConfirmErr(true)
+            setErrorMsg('Passwords must match')
+        } else if (checkEmail(email.value)){
+            setEmailNotFound(true)
+            setErrorMsg('Email already exists') 
         } else if (checkUsername(tag_name.value)){
-            setUsernameErr(true)
-            setEmailErr(false)
+            setUsernameNotFound(true)
+            setErrorMsg('Username is already taken')
         } else {
             new_user(
             {variables: {
                 tag_name: tag_name.value,
-                first_name: first_name.value,
-                last_name: last_name.value,
-                email: email.value,
+                first_name: first_name.value.toLowerCase(),
+                last_name: last_name.value.toLowerCase(),
+                email: email.value.toLowerCase(),
                 pass: password.value,
                 birth_date: birth_date,
                 pfp_url: 'url'
             }})
-            setEmailErr(false)
-            setUsernameErr(false)
             window.location.href = '/login'
         }
     }
@@ -107,8 +219,7 @@ const RegisterForm = () => {
         <div>
             <h1>Register</h1>
             <p>Enter your details below to continue</p>
-            {emailErr && <div className='error-msg'><p>Email is already in use</p></div>}
-            {usernameErr && <div className='error-msg'><p>Username is already taken</p></div>}
+            {(emailNotFound || emailNotValid || usernameNotFound || usernameNotValid || namesNotValid || ageError || passNotValid || confirmErr) && <ErrorMsg message={errorMsg}/>}
             <form className="entry-form flex-col-ctr" onSubmit={handleSubmit}>
                 <div className="reg-names-box">
                     <input type="text" ref={value => first_name = value} id='first_name' placeholder="First name" required/>
@@ -116,18 +227,7 @@ const RegisterForm = () => {
                 </div>
                 <div className="birthdt-reg-box">
                     <select id="year" ref={value => year = value} name="year">
-                        <option value="2010">2010</option>
-                        <option value="2011">2011</option>
-                        <option value="2012">2012</option>
-                        <option value="2013">2013</option>
-                        <option value="2014">2014</option>
-                        <option value="2015">2015</option>
-                        <option value="2016">2016</option>
-                        <option value="2017">2017</option>
-                        <option value="2018">2018</option>
-                        <option value="2019">2019</option>
-                        <option value="2020">2020</option>
-                        <option value="2021">2021</option>
+                        {selectYear.map(year => <option value={year} key={year}>{year}</option>)}
                     </select>
                     <select ref={value => month = value} id='month' name="month">
                         <option value="01">January</option>
@@ -143,12 +243,14 @@ const RegisterForm = () => {
                         <option value="11">November</option>
                         <option value="12">December</option>
                     </select>
-                    <input type="number" ref={value => day = value} id='day' name='day' required/>
+                    <select ref={value => day = value} id='day' name='day'>
+                        {selectDay.map(day => <option value={`${day}`} key={day}>{day}</option>)}
+                    </select>
                 </div>
-                <input type="email" ref={value => email = value} id='email' placeholder="Email" required/>
+                <input type="text" ref={value => email = value} id='email' placeholder="Email" required/>
                 <input type="text" ref={value => tag_name = value} id='tag_name' placeholder="Create your username" required/>
                 <input type="password" ref={value => password = value} id='password' placeholder="Password" required/>
-                <input type="password" placeholder="Confirm your password" required/>
+                <input type="password" ref={value => confirmpass = value}  id='confirmpass' placeholder="Confirm your password" required/>
                 <button className="entry-btn btn" type="submit">REGISTER</button>
             </form>
             <div className="entry-link flex-ctr">
