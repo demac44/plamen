7bdbc753bd26fb943e2c82d4a1d69e1c6ae9fe56
demac44 <demirumejr2@gmail.com>
2022-01-17 23:14:41 +0200

    Fixed logging out clearing data

--- controllers/logout.js
+++ controllers/logout.js
@@ -1,12 +1,11 @@
 import express from 'express'
-import auth from '../middleware/auth.js'
 
 const router = express.Router()
 
 
 router.post('/', (req, res) => {
     res.clearCookie("x-auth-token")
-    res.send('del')
+    res.sendStatus(200)
 })
 
 
