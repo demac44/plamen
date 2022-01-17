234dd4f622e351c8da8cde57fbc23370a71a9f2a
demac44 <demirumejr2@gmail.com>
2022-01-17 05:06:59 +0200

    Fixed chat

--- middleware/db.js
+++ middleware/db.js
@@ -1,20 +1,20 @@
 import {createConnection} from 'mysql2'
 
-const connection = createConnection({
-    host: "0.0.0.0",
-    port:"3306",
-    user: 'root',
-    database: 'socialmediapp',
-})
-
-
 // const connection = createConnection({
-//     host: "db4free.net",
+//     host: "0.0.0.0",
 //     port:"3306",
-//     user: 'demac22',
-//     password:'Windowjesubak2000',
-//     database: 'plamen_main',
+//     user: 'root',
+//     database: 'socialmediapp',
 // })
+
+
+const connection = createConnection({
+    host: "db4free.net",
+    port:"3306",
+    user: 'demac22',
+    password:'Windowjesubak2000',
+    database: 'plamen_main',
+})
 
 
 export default connection 
