0af0f34f1e541bbea6991f256e84da0808b0caf0
demac44 <demirumejr2@gmail.com>
2022-01-18 22:52:21 +0200

    Fixed chat overflow

--- middleware/db.js
+++ middleware/db.js
@@ -1,20 +1,20 @@
 import {createConnection} from 'mysql2'
 
-const connection = createConnection({
-    host: "0.0.0.0",
-    port:"3306",
-    user: 'root',
-    database: 'socialmediapp',
-})
-
-
 // const connection = createConnection({
-//     host: process.env.DB_HOST,
-//     port:process.env.DB_PORT,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASS,
-//     database: process.env.DB_NAME,
+//     host: "0.0.0.0",
+//     port:"3306",
+//     user: 'root',
+//     database: 'socialmediapp',
 // })
+
+
+const connection = createConnection({
+    host: process.env.DB_HOST,
+    port:process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+})
 
 
 export default connection 
