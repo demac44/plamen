bea4e5fd313604fdffa576081b3b5e800650a75d
demac44 <demirumejr2@gmail.com>
2022-01-20 05:42:16 +0200

    Fixed set time

--- client/src/components/General components/SetTime.js
+++ client/src/components/General components/SetTime.js
@@ -9,7 +9,7 @@ export default SetTime
 
 const getTime = (timestamp) => {
     let d = Math.floor((Date.now() - new Date(parseInt(timestamp)).getTime())/1000/60)
-    if(d===0) return 'Now'
+    if(d<=0) return 'Now'
     else if(d<60) return d+'m'
     else if(d>60 && d<60*24) return Math.floor(d/60)+'h'
     else if(d>60*24 && d<60*24*30) return Math.floor(d/(60*24))+'d'
